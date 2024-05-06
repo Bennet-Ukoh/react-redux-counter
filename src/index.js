690a5c64604f99e450536001859b84daeaf256a2
Bennet-Ukoh <bennetukoh@gmail.com>
2024-05-06 15:31:21 +0100

    redux functionality added

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import myReducers from "./reducers";
+import { legacy_createStore as createStore } from "redux";
 
-//create the store
-const myStore = configureStore(myReducers);
+//Create the store
+const myStore = createStore(myReducers);
 
 //This will console log the current state everytime the state changes
 myStore.subscribe(() => console.log(myStore.getState()));
 
 //Enveloping the App inside the Provider, ensures that the states in the store are available
 //throughout the application
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <Provider store={myStore}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
