80a7d472b85dad4f6e3dd2abceb2f15676a8262e
Bennet-Ukoh <bennetukoh@gmail.com>
2024-05-06 12:51:33 +0100

    added a new project using react-redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import myReducers from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//create the store
+const myStore = configureStore(myReducers);
+
+//This will console log the current state everytime the state changes
+myStore.subscribe(() => console.log(myStore.getState()));
+
+//Enveloping the App inside the Provider, ensures that the states in the store are available
+//throughout the application
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={myStore}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
